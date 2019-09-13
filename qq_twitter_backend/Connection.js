3f013edbb005ae7f37ee26a6fbd906a51cceb7ae
Filipe Nunez <filipebnunez@gmail.com>
2019-09-13 00:24:28 -0300

    First postgres request working in the browser

--- qq_twitter_backend/Connection.js
+++ qq_twitter_backend/Connection.js
@@ -1,7 +1,60 @@
-var express = require('express')
+var express = require(`express`)
 var app = express()
-function test(rest) {
-    app.get('/', function (req, res) {
+const { Client } = require(`pg`);
+const client = new Client({
+    user: "postgres",
+    password: "entersandman",
+    host: `localhost`,
+    port: 5432,
+    database: `qq_twitter`
+});
+
+
+async function connect() {
+    try {
+        await client.connect()
+        console.log("Connection Sucess")
+    }
+    catch (e) {
+        console.error(`Failed Connection ${e}`)
+    }
+}
+
+async function disconnect() {
+    try {
+        await client.end()
+    }
+    catch (e) {
+        console.error(`Failed to disconnect ${e}`)
+    }
+}
+
+async function query(table, row) {
+    try {
+        return result = await client.query(`select * from ${table} where login = $1`, [row])
+    }
+    catch (e) {
+        console.error(`Failed to query ${e}`)
+    }
+}
+
+async function QueryLoginData(login) {
+    await connect()
+    var result = await query(`user_data`, login)
+    console.table(result.rows)
+    await disconnect()
+    return result
+}
+
+var result = QueryLoginData('fnunez')
+result.then(function (data) {
+    var x = data.rows[0].login
+    apiLogRequest(x)
+})
+
+
+function apiLogRequest(rest) {
+    app.get(`/`, function (req, res) {
         res.send(rest)
     })
     var server = app.listen(8081, function () {
@@ -12,48 +65,3 @@ function test(rest) {
     })
 
 }
-
-const { Client } = require('pg');
-
-const client = new Client({
-    user: "postgres",
-    password: "entersandman",
-    host: 'localhost',
-    port: 5432,
-    database: 'qq_twitter'
-});
-
-
-async function QueryLoginData(login) {
-    var result = null
-    try {
-        await client.connect()
-        console.log("Connected")
-        result = await client.query("select * from user_data where login = $1", [login])
-        console.table(result.rows)
-        // console.log(result.rows[0].login)
-    }
-    catch{
-        e => console.log(e)
-    }
-    finally {
-        await client.end()
-        console.log("Connection ended")
-    }
-    // console.log(result.rows[0].login)
-    return result
-    // return result;
-}
-
-var result = QueryLoginData('fnunez')
-result.then(function (data) {
-    var x = data.rows[0].login
-    test(x)
-})
-
-// client.connect()
-//     .then((console.log("Connected")))
-//     .then(() => client.result("select * from user_data"))
-//     .then(result => console.table(result.rows))
-//     .catch(e => console.log(e))
-//     .finally(() => client.end())
